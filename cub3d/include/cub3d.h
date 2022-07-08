57fabbb9f34dfb6a36ffb24f958434a9ec663f59
Maxime <maxime@nc-ass-vip.sdv.fr>
2022-07-09 00:39:36 +0200

    cub3d

---
+++ cub3d/include/cub3d.h
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabriel <mabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 12:15:07 by kychoi            #+#    #+#             */
+/*   Updated: 2022/07/08 23:50:14 by mabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# define RED "\033[0;31m"
+# define GRN "\033[0;32m"
+# define YEL "\033[0;33m"
+# define BLU "\033[0;34m"
+# define MAG "\033[0;35m" //purple
+# define CYN "\033[0;36m" //emerald
+# define WHT "\033[0;37m"
+# define DFT "\033[0m"
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <math.h>
+
+/* libft */
+# include "libft.h"
+/* mlx */
+# include "mlx.h"
+
+/* Pointer size */
+# define P_SIZE 8
+# define OFFMAP -1
+# define INMAP -2
+# define OUTMAP -3 
+# define ERROR_LINE 1
+# define ERROR_MAP 2
+
+typedef enum e_bool { FALSE, TRUE }	t_bool;
+typedef enum e_error { ERR_PARSE = -42, ERR_MAP = 1, ERR_LINE = 2 }	t_error;
+typedef enum e_event { EVENT_EXIT_WIN = 17 }	t_event;
+
+//TODO: parler avec Max pour des variables dans s_player et t_game
+typedef struct s_player {
+	int		x;
+	int		y;
+}	t_player;
+
+//Principal struct
+typedef struct s_game {
+	void		*mlx;
+	void		*win;
+	char		*no_path;
+	char		*so_path;
+	char		*we_path;
+	char		*ea_path;
+	//FIXME: int[3] or char * for t
+	char		*f_color;
+	//FIXME: int[3] or char * for t
+	char		*c_color;
+	char		**map;
+	t_player	*player;
+	int			count;
+	int 		line;
+	int			err;
+}	t_game;
+
+/* parse.c */
+int		parse(char **av, t_game *game);
+int		check_place(t_game *game, int place);
+void	error_parsing(t_error err_code, char *msg, t_game *game);
+int		validate_line(char *line, t_game *game);
+void	exit_error_file(t_game *game, int err_code);
+int		open_file(char *s, t_game *game);
+void	init_parse(t_game *game);
+int	forest_help(t_game *game, int code);
+
+/* painter.c */
+void	put_header(void);
+void	print_game(t_game *game);
+
+/* free.c */
+void	free_game(t_game *game);
+
+#endif
---
+++ cub3d/src/painter.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   painter.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 19:08:13 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/07/07 17:23:46 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	print_game(t_game *game)
+{
+	int i;
+
+	printf("\n[%p]game\n", game);
+	printf("\n  [%p]game->mlx\n", game->mlx);
+	printf("\n  [%p]game->win\n", game->win);
+	printf("\n  [%p]game->no_path:%s\n", game->no_path, game->no_path);
+	printf("\n  [%p]game->so_path:%s\n", game->so_path, game->so_path);
+	printf("\n  [%p]game->we_path:%s\n", game->we_path, game->we_path);
+	printf("\n  [%p]game->ea_path:%s\n", game->ea_path, game->ea_path);
+	printf("\n  [%p]game->f_color:%s\n", game->f_color, game->f_color);
+	printf("\n  [%p]game->c_color:%s\n", game->c_color, game->c_color);
+	printf("\n  [%p]game->map:\n", game->map);
+	i = 0;
+	while (game->map && game->map[i])
+	{
+		printf("    [%p]game->map[%d]%s\n", game->map[i], i, game->map[i]);
+		++i;
+	}
+}
+
+
+//TODO: change to CUBE\n3D
+void	put_header(void)
+{
+	printf(
+		MAG"/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n\n"
+		RED"     .^. .  _    \n"
+		YEL"    /: ||`\\/ \\\\~  ,       \n"
+		GRN"  , [   &    / \\ y'      ###   ###  ####   ###    #  ####   \n"
+		CYN" {v':   `\\   / `&~-,    #  # #  #    #    #  #   #    #    \n"
+		BLU"'y. '    |`   .  ' /   #   #   #    #    #   #  #    # \n"
+		MAG" \\   '  .       , y   #       #   ####  #    ###   #### \n"
+		RED" v .        '     v   \n"
+		YEL" V  .~.      .~.  V    ######  #     # ###### #      # \n"
+		GRN" : (  0)    (  0) :   /_____  #_____# #_____ #      # \n"
+		CYN"  i `'`      `'` j         / #     # #      #      # \n"
+		BLU"   i     __    ,j    ###### #     # ###### ###### ###### \n"
+		MAG"    `\\%%`~....~'&         \n"
+		RED" <~o' /  \\/` \\-s,                This is NOT a big shell \n"
+		YEL"  o.~'.  )(  r  .o ,.  \n"
+		GRN" o',  %%``\\/``& : 'bF  \n"
+		CYN"d', ,ri.~~-~.ri , +h  \n"
+		BLU"`oso' d`~..~`b 'sos`                          Created by \n"
+		MAG"     d`+ II +`b                       mabriel and kychoi \n"
+		RED"     i_:_yi_;_y                              at 42 Paris \n\n"
+		YEL"/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n"
+		GRN);
+}
+
+//RED -> YEL -> GRN -> CYN -> BLU -> MAG
+/*
+
+   ###   ###  ####   ###    #  ####   
+  #  # #  #    #    #  #   #    #    
+ #   #   #    #    #   #  #    #  
+#       #   ####  #    ###   #### 
+
+    ######   #    # #####  #      #
+   /_____   #____# #____  #      #
+        /  #    # #      #      #
+  ######  #    # ###### ###### ######
+
+              This is NOT a big shell
+
+
+
+                           Created by
+		           mabriel and kychoi
+				          at 42 Paris
+						  
+*/
