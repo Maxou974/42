d6c284f9ba75d2b0b5763fabe5844bd5d41c8e45
Maxime <maxime@nc-ass-vip.sdv.fr>
2022-07-09 04:19:11 +0200

    cub3d parsing avance

---
+++ cub3d/src/parsing/tokenize_map_help.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize_map_help.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabriel <mabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 02:19:45 by mabriel           #+#    #+#             */
+/*   Updated: 2022/07/09 03:40:21 by mabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	get_vertical_size(char **s)
+{
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
+}
+
+int get_horizontal_size(char **s)
+{
+	int	max;
+	int	i;
+	int	j;
+	
+	max = 0;
+	i = 0;
+	while (s && s[i])
+	{
+		j = 0;
+		while (s && s[i][j])
+			j++;
+		if (j > max)
+			max = j;
+		i++;
+	}
+	return (max);
+}
