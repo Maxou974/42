89dc494d55c18d009b45c3115080a12da34a52a7
Maxou974 <maximeabriel974@gmail.com>
2023-07-13 14:28:38 +0200

    Inception and remove webserv

--- Inception/srcs/requirements/wordpress/tools/config.bash
+++ Inception/srcs/requirements/wordpress/tools/config.bash
@@ -8,7 +8,7 @@ wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD 
 
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --path="/var/www/wordpress/" --allow-root
 
-wp user create $WORDPRESS_DFT_USER $WORDPRESS_DFT_EMAIL --role=author --user_pass=$WORDPRESS_DFT_PASSWORD --allow-root
+wp user create $WORDPRESS_DFT_USER $WORDPRESS_DFT_EMAIL --role=author --user_pass=$WORDPRESS_DFT_PASSWORD --allow-root --path="/var/www/wordpress/"
 fi
 
 exec /usr/sbin/php-fpm7.3 -F
