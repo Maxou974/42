70f391b11619e6bb92b0368c7e2bfde0bcbca3e5
Maxou974 <maximeabriel974@gmail.com>
2024-03-07 12:08:16 +0100

    finish testing

--- outcircle/libasm/main.c
+++ outcircle/libasm/main.c
@@ -66,18 +66,139 @@ void	bonus();
 
 int main()
 {
-	mandatory();
-	// bonus();
+	// mandatory();
+	bonus();
+	return 0;
+}
+
+void atoi_base_test(char *src, char* base, int result) {
+	int res = ft_atoi_base(src, base);
+
+	if (res != result)
+		printf("ERROR: ft_atoi_base src: %s, base: %s, returned: %d, expected: %d\n", \
+						src, base, res, result);
+}
+
+int cmp(void *data, void* d2) {
+	// printf("%d\n", *(char*)data);
+	if (*(char*)data > *(char*)d2)
+		return 1;
+	return 0;
+}
+
+char *strcat_list(t_list *list) {
+	char *dst = malloc(20);
+	memset(dst, 0, 20);
+
+	while (list) {
+		strcat(dst, list->data);
+		list = list->next;
+	}
+	return dst;
+}
+
+int is_odd(void *d1) {
+	if (*(char *)d1 % 2 == 1)
+		return 0;
+	return 1;
+}
+
+int returnzero() {
 	return 0;
 }
 
 void bonus() {
+	atoi_base_test("10", "01", 2);
+	atoi_base_test("-10", "01", -2);
+	
+	atoi_base_test("10", "0123456789", 10);
+	atoi_base_test("12e31", "0123456789", 0);
+	
+	atoi_base_test("ea", "abcdefhijk", 40);
+	atoi_base_test("    -ea", "abcdefhijk", -40);
+	atoi_base_test(" - ea", "abcdefhijk", 0);
+	atoi_base_test("zyjk", "abcdefhijk", 0);
 
+	atoi_base_test("40", "0123456789abcdef", 64);
+	atoi_base_test("f", "0123456789abcdef", 15);
+
+	t_list *list = NULL, *tmp;
+
+	ft_list_push_front(&list, ft_strdup("World"));
+	ft_list_push_front(&list, ft_strdup("[ ]"));
+	ft_list_push_front(&list, ft_strdup("Hello"));
+
+	char *dst;
+
+	dst = strcat_list(list);
+	int compare = ft_strcmp(dst, "Hello[ ]World");
+	if (compare)
+		printf("ERROR push_front\n");
+	free(dst);
+
+	tmp = NULL;
+
+	if (ft_list_size(list) != 3)
+		printf("ERROR list_size\n");
+	if (ft_list_size(tmp) != 0)
+		printf("ERROR list_size\n");
+
+	ft_list_sort(&list, &cmp);
+
+	dst = strcat_list(list);
+	compare = ft_strcmp(dst, "HelloWorld[ ]");
+	if (compare)
+		printf("ERROR push_front\n");
+	free(dst);
+
+
+	ft_list_remove_if(&list, "Hello", &ft_strcmp, &free);
+
+	dst = strcat_list(list);
+	compare = ft_strcmp(dst, "World[ ]");
+	if (compare)
+		printf("ERROR push_front\n");
+	free(dst);
+
+	ft_list_remove_if(&list, "World", &ft_strcmp, &free);
+	ft_list_remove_if(&list, "[ ]", &ft_strcmp, &free);
+
+	dst = strcat_list(list);
+	compare = ft_strcmp(dst, "");
+	if (compare)
+		printf("ERROR push_front\n");
+	free(dst);
+
+	if (ft_list_size(list) != 0)
+		printf("ERROR list_size\n");
+
+	ft_list_push_front(&list, strdup("1"));
+	ft_list_push_front(&list, strdup("2"));
+	ft_list_push_front(&list, strdup("3"));
+	ft_list_push_front(&list, strdup("4"));
+	ft_list_push_front(&list, strdup("5"));
+	ft_list_push_front(&list, strdup("6"));
+
+	dst = strcat_list(list);
+	if (strcmp(dst, "654321") != 0)
+		printf("ERROR list must be: 654321, but is: %s\n", dst);
+	free(dst);
+
+	ft_list_remove_if(&list, NULL, &is_odd, &free);
+
+	dst = strcat_list(list);
+	if (strcmp(dst, "642") != 0)
+		printf("ERROR list must be: 642, but is: %s\n", dst);
+	free(dst);
+
+	ft_list_remove_if(&list, NULL, &returnzero, &free); // remove all
 }
 
 
 void	mandatory() {
 	char dst[20];
+	memset(dst, 0, 20);
+	strcpy(dst, "Hello World");
 
 	test_strcmp("", "");
 	test_strcmp("A", "A");
@@ -95,16 +216,19 @@ void	mandatory() {
 	test_strcmp("whoeif", "ascno");
 	test_strcmp("whoeif", "Qscno");
 	test_strcmp("Qscno", "whoeif" );
+	test_strcmp("Hello World", dst);
 
+	test_strlen(dst);
 	test_strlen("");
 	test_strlen("Hello");
 	test_strlen("H");
 	test_strlen("wehjfoiwehfoiwehfowhefowhefuwefwuiefwiuefui");
 
-	test_strcpy(dst, "Hello World!");
+	test_strcpy(dst, "Hello World");
 	test_strcpy(dst, "");
 	test_strcpy(dst, "H");
 
+	test_strdup(dst);
 	test_strdup("");
 	test_strdup("H");
 	test_strdup("Hello World!");
--- outcircle/libasm/src/ft_strcmp.s
+++ outcircle/libasm/src/ft_strcmp.s
@@ -3,19 +3,22 @@
 
 ft_strcmp:
 	xor rcx, rcx				; rcx = 0; i = 0
+	jmp start_loop
+	
+increment:
+	inc rcx
 
-loop:
+start_loop:
 	mov al, [rsi + rcx]			; tmp = src[i]
 	cmp al, [rdi + rcx]			; compare tmp and dst[i]
 	jnz return					; Jump if Not Zero to return
 
 is_end_of_string:
-	xor r8, r8					; r8 = 0
-	add r8, [rdi + rcx]			; r8 += dst[i]
-	add r8, [rsi + rcx]			; r8 += src[i]
-	inc rcx						; i += 1
+	movzx r8, byte [rdi + rcx]			; r8 += dst[i]
+	movzx r9, byte [rsi + rcx]			; r8 += src[i]
+	add r8, r9
 	cmp r8, 0					; compare r8 and 0
-	jnz loop					; Jump if Not Zero to loop
+	jnz increment					; Jump if Not Zero to loop
 
 return:
 	movzx rax, byte [rdi + rcx]	; rax = dst[i]
